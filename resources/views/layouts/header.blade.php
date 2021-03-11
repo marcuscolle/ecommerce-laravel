d085413c6aa488cdff8dd854940906750a2056d9
Marcus <collemarcus@hotmail.com>
2021-03-11 17:53:18 +0000

    tryig to fix when page load on live, css/bootstap not working error givin on console

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,8 +7,8 @@
     <meta name="author" content="">
     <title>Home | E-Shopper</title>
 
-	<link href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    
+	
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/prettyPhoto.css') }}" rel="stylesheet">
     <link href="{{ asset('css/price-range.css') }}" rel="stylesheet">
